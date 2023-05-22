d69bcd9a528b5f6c4d8ad7061e085110e8c2fe35
Jacob Rodal <dev+github@jrodal.com>
2023-05-22 00:37:19 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="tldrwl",
-    version="0.0.5",
+    version="0.0.6",
     author="Jacob Rodal",
     author_email="dev@jrodal.com",
     description="Too long, didn't read/watch/listen",
